18805f9b1a096e8beba7d50ef4c52299bd3539a3
CuongZ <34135258+CuongZ@users.noreply.github.com>
2018-12-05 20:33:59 +0700

    Update Install (bat).bat

--- Install (bat).bat
+++ Install (bat).bat
@@ -1,48 +1,64 @@
-color 0A
-;Made by CuongZ
+color f0
 @echo off
-@echo Preparing for app...
-@echo off
-@echo Checking adb compoment...
-adb
+echo Checking ADB compoments...
+.\content\adb.exe
 cls
 @echo off
-@echo Killing current adb server...
-adb kill-server
-@echo off
-@echo Running adb server...
-adb start-server
+echo Killing current ADB server...
+.\content\adb kill-server
 cls
 @echo off
-echo [App name]APK Installer for Tencent Gaming Buddy
-pause
-cls
-@echo off
-echo [NOTE] Please confirm that you've run Tencent Gaming Buddy's PUBGM emulator first and follow below step!
-echo [NOTIFICATION] Enable developer setting and USB debugging like in this video: https://youtu.be/ksQB_eniDYU
-pause
-adb kill-server
-@echo off
-echo [STEP1] Make sure you have downloaded APKs to install ! 
-pause
-@echo off
-echo [STEP2] Rename it to: app.apk
-pause
-@echo off
-@echo [NOTIFICATION] [USER STEP] Now you can see your emulator with named ' emulatorxxxx emulator ' ? If it not displayed, please check your emulator. If it displayed, press any key to continue.
-adb kill-server
-adb devices
+echo  =============Script Install APK - Tencent Gaming Buddy =====================
+echo = (c) Made by CuongZ                                                        =
+echo = Github : https://github.com/CuongZ                                        =
+echo = Facebook : https://facebook.com/dtcuong205                                =
+echo = Twitter: https://twitter.com/dtcuong205                                   =
+echo = This script include ADB command-line.                                     =                                                                                                                       
+echo = Hit enter to begin!                                                       =
+echo =============================================================================
 pause
 cls
 @echo off
-@echo Now installing your app...
-adb install app.apk
-@echo off
-@echo Finished app install !
-@echo off
-@echo You're done! Enjoy your app!
-@echo off
-echo [Copyright](c) CuongZ
+echo ============================================================================================================
+echo =[NOTE] Please confirm that you've run Tencent Gaming Buddy's PUBGM emulator first and follow below step!  =
+echo =[NOTIFICATION] Enable developer setting and USB debugging like in this video: https://youtu.be/ksQB_eniDYU=
+echo ============================================================================================================
 pause
+.\content\adb kill-server
+cls
+@echo off
+echo =========================================================
+echo =[STEP1] Make sure you have downloaded APKs to install != 
+echo =========================================================
+pause
+cls
+@echo off
+echo ===============================
+echo =[STEP2] Rename it to: app.apk=
+echo ===============================
+pause
+cls
+@echo off
+echo ===============================================================================================================================================================================================
+echo =[NOTIFICATION] [USER STEP] Now you can see your emulator with named ' emulatorxxxx emulator ' ? If it not displayed, please check your emulator. If it displayed, press any key to continue. =
+echo ===============================================================================================================================================================================================
+.\content\adb kill-server
+.\content\adb devices
+pause
+cls
+@echo off
+echo ============================
+echo =Now installing your app...=
+echo ============================
+.\content\adb install ./app.apk
+cls
+@echo off
+echo ==========================
+echo = Finished app install ! =
+echo ==========================
+@echo off
+echo ================================
+echo = You're done! Enjoy your app! =
+echo ================================
 exit
 end
