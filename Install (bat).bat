6973d783edea83a1346099519f3f8ac0f43119b5
CuongZ <34135258+CuongZ@users.noreply.github.com>
2019-03-01 20:31:12 +0700

    Version 2.0 <3
    Do vài vấn đề với ver 1.1 nên tôi bhop sang ver 2.0 với nhiều cải tiến hơn ;) Và cũng cảm ơn một số thành viên của J2TeaM Community đã đào bài này lên để tôi còn update <3

--- Install (bat).bat
+++ Install (bat).bat
@@ -1,17 +1,14 @@
-color f0
-@echo off
-echo Checking ADB compoments...
-adb
-cls
+color 0a
+title APK Install For Tencent Gaming Buddy - Version 2.0
 @echo off
 echo Killing current ADB server...
-adb kill-server
+.\content\adb kill-server
 cls
 @echo off
 echo  =============Script Install APK - Tencent Gaming Buddy =====================
-echo = (c) Made by CuongZ                                                        =
+echo = (c) Made by CuongZ. Version 2.0                                           =
 echo = Github : https://github.com/CuongZ                                        =
-echo = Facebook : https://facebook.com/dtcuong205                                =
+echo = Facebook : https://facebook.com/dtcuong205.real                           =
 echo = Twitter: https://twitter.com/dtcuong205                                   =
 echo = This script include ADB command-line.                                     =                                                                                                                       
 echo = Hit enter to begin!                                                       =
@@ -24,41 +21,40 @@ echo =[NOTE] Please confirm that you've run Tencent Gaming Buddy's PUBGM emulato
 echo =[NOTIFICATION] Enable developer setting and USB debugging like in this video: https://youtu.be/ksQB_eniDYU=
 echo ============================================================================================================
 pause
-adb kill-server
 cls
 @echo off
-echo =========================================================
-echo =[STEP1] Make sure you have downloaded APKs to install != 
-echo =========================================================
+echo =====================================================================================================
+echo =Make sure you have downloaded APK to install ! And put it in the same location the file Install.bat=
+echo =====================================================================================================
 pause
 cls
+@echo off
+echo =====================================================================
+echo =Now you can see your emulator with named ' emulatorxxxx emulator '?=
+echo =====================================================================
+@echo off
+echo =====================================================================================================
+echo =If it not displayed, please check the step above again. If it displayed, press any key to continue.=
+echo =====================================================================================================
+.\content\adb kill-server
+.\content\adb devices
+pause
+cls
+set appname= Default
 @echo off
 echo ===============================
-echo =[STEP2] Rename it to: app.apk=
+echo = Please enter your app name! =
 echo ===============================
+set /p appname=
+@echo off
+echo =======================================
+echo =Now installing your app ( %appname% )=
+echo =======================================
+.\content\adb install ./%appname%
+cls
 pause
-cls
 @echo off
-echo ===============================================================================================================================================================================================
-echo =[NOTIFICATION] [USER STEP] Now you can see your emulator with named ' emulatorxxxx emulator ' ? If it not displayed, please check your emulator. If it displayed, press any key to continue. =
-echo ===============================================================================================================================================================================================
-adb kill-server
-adb devices
+echo ==========================================
+echo = Finished app install ! Enjoy your app! =
+echo ==========================================
 pause
-cls
-@echo off
-echo ============================
-echo =Now installing your app...=
-echo ============================
-adb install ./app.apk
-cls
-@echo off
-echo ==========================
-echo = Finished app install ! =
-echo ==========================
-@echo off
-echo ================================
-echo = You're done! Enjoy your app! =
-echo ================================
-exit
-end
